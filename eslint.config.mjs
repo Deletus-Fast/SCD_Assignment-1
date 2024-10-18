73c32047b353fea2d5bf9834b976ba20d301eebd
Talal Ahmed Khan <93150072+Deletus-Fast@users.noreply.github.com>
2024-10-18 22:16:11 +0500

    update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,6 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import pluginReact from "eslint-plugin-react";
-
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+  {languageOptions: { globals: globals.node }},
+  {files: ["test/page.test.js"], languageOptions: { globals: globals.browser }},
 ];
